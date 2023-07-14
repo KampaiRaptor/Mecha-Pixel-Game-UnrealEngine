3871aa89f75505414db063265f2e35a379a14c08
Fansi129 <120315901+Fansi129@users.noreply.github.com>
2023-07-14 10:58:05 +0200

    Project Origin

---
+++ Source/MechaJam/MechaJamGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MechaJamGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MECHAJAM_API AMechaJamGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
